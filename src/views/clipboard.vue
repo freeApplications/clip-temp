ba50fe5f6f4e21fc176e9b6c2da8fee5d8573cae
yutaka-sugao <yutaka.sugao+github@gmail.com>
2022-03-06 14:32:32 +0900

    :lipstick: Visualize selected item in clipboard histories

--- src/views/clipboard.vue
+++ src/views/clipboard.vue
@@ -1,16 +1,24 @@
 <template lang="pug">
 #clipboard
-  .list
-    .item(v-for="item in histories" :key="item.time")
+  .list(ref="list")
+    .item(
+      v-for="(item, index) in histories"
+      :key="item.time"
+      :class="{ selected: selectIndex === index }"
+      @click="selectIndex = index"
+    )
       | {{ item.text }}
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, computed } from 'vue';
+import { defineComponent, reactive, toRefs, ref, computed, watch } from 'vue';
 import { Clipboard } from '~/@types';
+import { HANDLING_KEYS } from '~/renderer-constants';
+import store from '~/store';
 
 type State = {
   histories: Clipboard[];
+  selectIndex: number;
 };
 export default defineComponent({
   setup() {
@@ -19,14 +27,58 @@ export default defineComponent({
     api.deliverClipboard((histories) => (state.histories = histories));
     const state = reactive<State>({
       histories: [],
+      selectIndex: 0,
     });
+    const { selectIndex } = toRefs(state);
+    const list = ref<HTMLDivElement>();
     const histories = computed(() => {
       return state.histories.slice().sort((a, b) => {
         return b.time - a.time;
       });
     });
+    watch(
+      () => store.state.keyEvent,
+      (keyEvent) => {
+        if (state.histories.length <= state.selectIndex || keyEvent === null) {
+          return;
+        }
+        const maxIndex = state.histories.length - 1;
+        switch (keyEvent.key) {
+          case HANDLING_KEYS.UP:
+            state.selectIndex > 0
+              ? state.selectIndex--
+              : (state.selectIndex = maxIndex);
+            break;
+          case HANDLING_KEYS.DOWN:
+            state.selectIndex < maxIndex
+              ? state.selectIndex++
+              : (state.selectIndex = 0);
+            break;
+          default:
+            return;
+        }
+        const refsList = list.value;
+        if (!refsList) return;
+        const item: Element = refsList
+          .querySelectorAll('.item')
+          .item(state.selectIndex);
+        const listTop = refsList.getBoundingClientRect().top;
+        const scrollBottom = refsList.scrollTop + refsList.clientHeight;
+        const itemRect = item.getBoundingClientRect();
+        const itemTop = itemRect.top - listTop + refsList.scrollTop;
+        const itemBottom = itemRect.bottom - listTop + refsList.scrollTop;
+        if (itemTop < refsList.scrollTop) {
+          refsList.scrollTop = itemTop - 1;
+        }
+        if (itemBottom > scrollBottom) {
+          refsList.scrollTop = itemBottom - refsList.clientHeight - 2;
+        }
+      }
+    );
     return {
       histories,
+      selectIndex,
+      list,
     };
   },
 });
@@ -50,6 +102,9 @@ export default defineComponent({
     text-align: left;
     &:not(:last-child) {
       border-bottom: 1px solid lightgray;
+    }
+    &.selected {
+      background-color: skyblue;
     }
   }
   &::-webkit-scrollbar {
